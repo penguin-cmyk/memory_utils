df369e9c2d3a97a98dab49e1cbe14fc2227d524b
penguin-cymk <slomogurl37@gmail.com>
2025-05-26 20:28:40 +0200

    Added docs & homepage

--- src/process.rs
+++ src/process.rs
@@ -79,6 +79,7 @@ impl Process {
     ///
     /// # Arguments
     /// * `exit_code` - The exit code you want to exit with. If none is provided it will default to `1`
+    ///
     pub fn terminate(&self, exit_code: Option<u32>) -> Result<(), Error> {
         unsafe  {
             let handle = OpenProcess(PROCESS_TERMINATE, 0, self.pid);
